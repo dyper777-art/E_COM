4e08bc2ee18652397e7294ebbc961dbc17d86ac8
ME <me@192.168.1.200>
2025-10-27 17:53:22 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -107,10 +107,15 @@ Route::middleware(['auth', 'verified', IsAdmin::class])->group(function () {
 
 
 
-Route::get(['/', '/home'], function () {
+Route::get('/', function () {
     $user = Auth::user();
     return view('user.index', compact('user'));
 })->name('home');
+
+Route::get('/home', function () {
+    $user = Auth::user();
+    return view('user.index', compact('user'));
+});
 
 
 Route::get('/product', function () {
