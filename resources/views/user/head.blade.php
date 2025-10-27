e9a66b707f5113947037ed19f570f516e5d3e111
ME <me@192.168.1.200>
2025-10-27 17:17:38 +0700

    commit

--- resources/views/user/head.blade.php
+++ resources/views/user/head.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
 
-    <meta name="title" content="COZA STORE"></title>
+    <title>COZA STORE</title>
     <meta content="COZE - Clothes Shop" name="description">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
