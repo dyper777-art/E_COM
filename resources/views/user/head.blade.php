520febcb6bfba8bcda5ec3446fa8fae8d83c2d9c
ME <me@192.168.1.200>
2025-10-27 17:32:23 +0700

    commit

--- resources/views/user/head.blade.php
+++ resources/views/user/head.blade.php
@@ -2,6 +2,7 @@
     <meta charset="UTF-8">
 
     <title>COZA STORE</title>
+    <meta property="og:title" content="Rick Astley - Never Gonna Give You Up (Official Video) (4K Remaster)">
     <meta content="COZE - Clothes Shop" name="description">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
